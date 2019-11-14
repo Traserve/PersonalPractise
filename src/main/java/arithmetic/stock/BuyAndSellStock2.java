923d506c49ca3e84a0c255bc32abb0940d028e35
cao.zhuang <cao.zhuang@szrttx.com>
2019-11-14 16:25:17 +0800

    股票问题

--- src/main/java/arithmetic/stock/BuyAndSellStock2.java
+++ src/main/java/arithmetic/stock/BuyAndSellStock2.java
@@ -82,9 +82,8 @@ public class BuyAndSellStock2 {
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
-            int temp = dp[i][0];
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], temp - prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
         }
         return dp[prices.length - 1][0];
     }
--- src/main/java/arithmetic/stock/BuyAndSellStockTransactionFee.java
+++ src/main/java/arithmetic/stock/BuyAndSellStockTransactionFee.java
@@ -9,4 +9,22 @@ package arithmetic.stock;
 
 public class BuyAndSellStockTransactionFee {
 
+    public static void main(String[] args) {
+        int[] prices = {1, 3, 2, 8, 4, 9};
+        System.err.println(maxProfit(prices, 2));
+    }
+
+    public static int maxProfit(int[] prices, int fee) {
+        if(prices.length == 0 || prices.length == 1){
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        }
+        return dp[prices.length-1][0];
+    }
 }
