4d05686beaa6462c6ba56b8104956ad438011189
cao.zhuang <cao.zhuang@szrttx.com>
2019-12-03 18:28:19 +0800

    //

--- src/main/java/arithmetic/LongestValidParentheses.java
+++ src/main/java/arithmetic/LongestValidParentheses.java
@@ -5,6 +5,18 @@ import java.util.Stack;
 /**
  * Description: https://leetcode.com/problems/longest-valid-parentheses/
  *
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ *
+ * Example 2:
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ *
  * @author Cao.Zhuang
  * @date 2019/11/18 11:57
  */
@@ -14,52 +26,28 @@ public class LongestValidParentheses {
     public static void main(String[] args) {
 //        System.err.println(longestValidParentheses("(())(()"));
 //        System.err.println(longestValidParenthesesDp("(())(()"));
+//        System.err.println(longestValidParenthesesDp2("(())(()"));
         System.err.println(longestValidParentheses2("(())(()"));
     }
 
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push('(');
-            } else if (!stack.empty() && stack.peek() == '(') {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-        return stack.empty();
-    }
-    public static int longestValidParentheses2(String s) {
-        int maxlen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 2; j <= s.length(); j+=2) {
-                System.err.println(s.substring(i, j));
-                if (isValid(s.substring(i, j))) {
-                    maxlen = Math.max(maxlen, j - i);
-                }
-            }
-        }
-        return maxlen;
-    }
-
+    /**
+     * 堆栈处理
+     * @param s
+     * @return
+     */
     public static int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<Integer>();
         int max = 0;
-        int left = -1;
-        for (int j = 0; j < s.length(); j++) {
-            if (s.charAt(j) == '(') {
-                stack.push(j);
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
             } else {
-                if (stack.isEmpty()) {
-                    left = j;
-                } else {
-                    stack.pop();
-                    if (stack.isEmpty()) {
-                        max = Math.max(max, j - left);
-                    } else {
-                        max = Math.max(max, j - stack.peek());
-                    }
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else {
+                    max = Math.max(i - stack.peek(), max);
                 }
             }
         }
@@ -67,10 +55,45 @@ public class LongestValidParentheses {
     }
 
     /**
-     * dp[i] represents number of valid parentheses matches from S[j to i] (j<i) dp[i] = V[i-1] + 2 + previous matches
-     * V[i- (V[i-1] + 2) ] if S[i] = ')' and '(' count > 0
+     * 两种情况：只检测右括号的前一位，()或))
+     * if: s[i]==')' && s[i-1]=='('
+     * then: dp[i] = dp[i-2] + 2
+     *
+     * if: s[i]==')' && s[i-1]==')'
+     * 判断内括号是否成立 if: s[i-1-dp[i-1]] == '('  then: dp[i] = dp[i-2-dp[i-1]] + dp[i-1] + 2
+     *
+     * 补充： it is no need to consider the condition "s[i-1] == '('"
+     * since "s[i-longest[i-1]-1] == '('" actually concludes this case.
+     * We could just use "if (i-1>=0 && i-longest[i-1]-1 >=0 && s[i-longest[i-1]-1] == '(')"
+     * @param s
+     * @return
      */
     public static int longestValidParenthesesDp(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return 0;
+        }
+        int max = 0;
+        int[] dp = new int[s.length() + 1];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i + 1] = dp[i - 1] + 2;
+                } else {
+                    int subLeft = i - 1 - dp[i];
+                    if (subLeft >= 0 && s.charAt(subLeft) == '(') {
+                        dp[i + 1] = dp[i - 1 - dp[i]] + dp[i] + 2;
+                    }
+                }
+                max = Math.max(max, dp[i + 1]);
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 结合堆栈和上面dp两种思路的解法
+     */
+    public static int longestValidParenthesesDp2(String s) {
         int[] dp = new int[s.length()];
         int open = 0;
         int max = 0;
@@ -93,5 +116,41 @@ public class LongestValidParentheses {
             }
         }
         return max;
+    }
+
+    /**
+     * 仅使用两个变量来存储
+     * @param s
+     * @return
+     */
+    public static int longestValidParentheses2(String s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = Math.max(maxlength, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
     }
 }
