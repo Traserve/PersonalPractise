0efac0c0fd61d17a8bd04efc1df939c23651e2c3
cao.zhuang <cao.zhuang@szrttx.com>
2019-12-05 16:59:47 +0800

    UniquePathsIII

---
+++ src/main/java/arithmetic/uniquePaths/UniquePathsIII.java
@@ -0,0 +1,98 @@
+package arithmetic.uniquePaths;
+
+/**
+ * Description: https://leetcode.com/problems/unique-paths-iii/
+ *
+ * On a 2-dimensional grid, there are 4 types of squares:
+ *
+ * 1 represents the starting square.  There is exactly one starting square.
+ * 2 represents the ending square.  There is exactly one ending square.
+ * 0 represents empty squares we can walk over.
+ * -1 represents obstacles that we cannot walk over.
+ * Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+ *
+ * Example 1:
+ *
+ * Input: [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+ * Output: 2
+ * Explanation: We have the following two paths:
+ * 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+ * 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+ *
+ * Example 2:
+ *
+ * Input: [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+ * Output: 4
+ * Explanation: We have the following four paths:
+ * 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+ * 2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+ * 3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+ * 4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+ *
+ * Example 3:
+ *
+ * Input: [[0,1],[2,0]]
+ * Output: 0
+ * Explanation:
+ * There is no path that walks over every empty square exactly once.
+ * Note that the starting and ending square can be anywhere in the grid.
+ *
+ *
+ * @author Cao.Zhuang
+ * @date 2019/12/5 10:01
+ */
+
+public class UniquePathsIII {
+
+    public static void main(String[] args) {
+        int[][] grid = {{1, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 2, -1}};
+        System.err.println(uniquePathsIII(grid));
+    }
+
+    static int res = 0, empty = 1, sx, sy, ex, ey;
+
+    public static int uniquePathsIII(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    empty++;
+                } else if (grid[i][j] == 1) {
+                    sx = i;
+                    sy = j;
+                } else if (grid[i][j] == 2) {
+                    ex = i;
+                    ey = j;
+                }
+            }
+        }
+        dfs(grid, sx, sy);
+        return res;
+    }
+
+    /**
+     * 暴力回溯
+     * @param grid
+     * @param x
+     * @param y
+     */
+    public static void dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] < 0) {
+            return;
+        }
+        if (x == ex && y == ey) {
+            if (empty == 0) {
+                res++;
+            }
+            return;
+        }
+        grid[x][y] = -2;
+        empty--;
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+        grid[x][y] = 0;
+        empty++;
+    }
+}
